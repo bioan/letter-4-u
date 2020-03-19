62399b346519c90dd503ad17e07c0eb0a318b7a7
Ioan Botez <85bioan@gmail.com>
2020-03-19 16:12:53 +0100

    Updates

---
+++ precache-manifest.5eb19dadf6c3a75edc750e4e3c61f9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc0e75d6501f13dba97f8b0eac9d1fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22dcc098a8e8534f840c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e6f91648091a71b87d11",
+    "url": "/static/js/2.644ddcce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.644ddcce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dcc098a8e8534f840c",
+    "url": "/static/js/main.1a5ae4fa.chunk.js"
+  },
+  {
+    "revision": "c9c49d05d74687058140",
+    "url": "/static/js/runtime-main.0c37c74b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
